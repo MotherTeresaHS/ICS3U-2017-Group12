22fb6a469adbaf0c622882ea8c7768a50208c589
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2018-01-09 12:41:39 -0500

    Scenes

---
+++ pause_scene.py
@@ -0,0 +1,70 @@
+# Created by: Mr. Coxall
+# Edited by;: Tochi kazi
+# Created on: Sep 2016
+# Created for: ICS3U
+# This scene shows the help scene.
+
+from scene import *
+import ui
+from Help_scene import*
+from Game_scene import*
+from main_menu_scene import *
+
+
+class PauseScene(Scene):
+    def setup(self):
+        # this method is called, when user moves to this scene
+        
+        center_of_screen = self.size/2
+        
+        # add background color
+        self.background = SpriteNode(position = self.size / 2, 
+                                     color = 'white', 
+                                     parent = self, 
+                                     size = self.size)
+                                     
+        self.brick_text = LabelNode(font = ('Chalkduster',72),
+                                    text = 'CREDIT',
+                                    color = 'white',
+                                    position = (self.size.x /2,self.size.y * 0.90),
+                                    parent = self)
+                                    
+                                    
+        self.start_button = LabelNode(text = 'Designed by: Tochi kazi',
+                                      font=('Chalkduster', 20),
+                                      parent = self,
+                                      position = self.size / 2,
+                                      scale = 0.75)
+                                      
+        back_button_position = self.size
+        back_button_position.x = 100
+        back_button_position.y = back_button_position.y - 100
+        self.back_button = SpriteNode('./assets/sprites/back_button.png',
+                                       parent = self,
+                                       position = back_button_position)
+                                       
+        
+    def update(self):
+        # this method is called, hopefully, 60 times a second
+        pass
+    
+    def touch_began(self, touch):
+        # this method is called, when user touches the screen
+        pass
+    
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        pass
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        
+        # if start button is pressed, goto game scene
+        if self.back_button.frame.contains_point(touch.location):
+            self.dismiss_modal_scene()
+
+
+
+
+
+

5662e25206de5f047ece9de5d0659ecee6128866
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2018-01-16 16:22:59 -0500

    Game project

---
+++ settings_scene.py
@@ -0,0 +1,78 @@
+# Created by: Mr. Coxall
+# Edited by: Tochi Kazi
+# Created on: Nov 2017
+# Created for: ICS3U
+# This scene shows a splash screen for 2 seconds,
+#   then transitions to the main menu.
+
+from scene import *
+import ui
+from Help_scene import*
+from Game_scene import*
+from settings_scene import*
+from main_menu_scene import*
+
+
+class SettingScene(Scene):
+    def setup(self):
+        # this method is called, when user moves to this scene
+        
+        # create timer, so that after 2 seconds move to next scene
+        self.start_time = time.time()
+        
+        # add MT blue background color
+        self.background = SpriteNode(position = self.size / 2, 
+                                     color = 'white', 
+                                     parent = self,
+                                     size = self.size)
+        self.setting_text = LabelNode(font = ('Chalkduster',72),
+                                      text = 'SETTINGS',
+                                      color = 'black',
+                                      position = (self.size.x/2, 700),
+                                      parent = self)
+        self.back_button = SpriteNode('./assets/sprites/back_button.png',
+                                     position = (200,700),
+                                     parent = self)
+        self.sound_button = SpriteNode('./assets/sprites/sound_button.png',
+                                      parent = self,
+                                      position = (300,400),
+                                      alpha = 0.5)
+                                      
+        
+    def update(self):
+        # this method is called, hopefully, 60 times a second
+        
+        # after 2 seconds, move to main menu scene
+        
+    
+     def touch_began(self, touch):
+        # this method is called, when user touches the screen
+        if self.sound_button.contains_point(touch.location):
+          self.sound_button.alpha = 0
+          self.soundoff_button = SpriteNode('./assets/sprites/soundoff_button.png',
+                                           parent = self,
+                                          position = (300,400))
+    
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        pass
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        if self.back_button.frame.contains_point(touch.location):
+            self.dismiss_modal_scene()
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass

e7d609416faf16a752b50128d9aec86b166962fc
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2018-01-23 21:00:37 -0500

    Edited game

---
+++ Game_level.py
@@ -0,0 +1,72 @@
+# Level data for the 'BrickBreaker' game
+
+colors = {'r': 'pzl:Red4', 'g': 'pzl:Green4', 'b': 'pzl:Blue4', 'y': 'pzl:Yellow4', 'p': 'pzl:Purple4'}
+
+level1 = '''
+ gggggggggggggggg
+ gggggggggggggggg
+ ggyyyyyyyyyyyygg
+ ggyrrrrrrrrrrygg
+ ggyrggggggggrygg
+ ggyrgyyyyyygrygg
+ ggyrgyrrrrygrygg
+ ggyrgyyyyyygrygg
+ ggyrggggggggrygg
+ ggyrrrrrrrrrrygg
+ ggyyyyyyyyyyyygg
+ gggggggggggggggg
+ gggggggggggggggg
+'''
+
+level2 = '''
+   yyyy
+  gggggg
+ gyy  yyg
+  
+ gyy  yyg
+  gggggg
+   yyyy
+   
+'''
+
+level3 = '''
+  pp   rr
+ pppp rrrr
+ pppp rrrr
+  pp   rr
+  
+  
+  gg   yy
+ gggg yyyy
+ gggg yyyy
+  gg   yy
+  
+'''
+
+level4 = '''
+   bbbb
+   b bb
+   bbbbyy
+     bbyy
+ bbbbbbyy
+ bbbbbbyy
+ bbyyyyyy
+ bbyyyyyy
+ bbyy
+ bbyyyy
+   yy y
+   yyyy
+   
+'''
+
+level5= '''
+   bbbbbbbb
+   yyy  yyy
+   bbbbbbbb
+   yyy
+   bbbbbbbb
+   bbb
+   
+'''
+
+levels = [level1, level2, level3, level4, level5]
---
+++ settings_scene.py
@@ -0,0 +1,104 @@
+# Created by: Mr. Coxall
+# Edited by: Tochi Kazi
+# Created on: Nov 2017
+# Created for: ICS3U
+# This scene shows a splash screen for 2 seconds,
+#   then transitions to the main menu.
+
+from scene import *
+import ui
+from Help_scene import*
+from Game_scene import*
+from settings_scene import*
+from main_menu_scene import*
+import sound 
+
+
+
+
+
+class SettingScene(Scene):
+    def setup(self):
+        # this method is called, when user moves to this scene
+        
+        # create timer, so that after 2 seconds move to next scene
+        
+        
+        # add MT blue background color
+        self.background = SpriteNode(position = self.size / 2, 
+                                     color = 'white', 
+                                     parent = self,
+                                     size = self.size)
+        self.setting_text = LabelNode(font = ('Chalkduster',72),
+                                      text = 'SETTINGS',
+                                      color = 'black',
+                                      position = (self.size.x /2,self.size.y * 0.90),
+                                      parent = self)
+        self.back_button = SpriteNode('./assets/sprites/back_button.png',
+                                     position = (self.size.x/4 * 0.60,self.size.y * 0.10),
+                                     parent = self)
+        self.sound_button = SpriteNode('./assets/sprites/sound_button.png',
+                                      parent = self,
+                                      position = (self.size.x/2 - 200,self.size.y/2 + 100),
+                                      alpha = 1)
+        self.soundoff_button = SpriteNode('./assets/sprites/soundoff_button.png',
+                                           parent = self,
+                                           position = (self.size.x/2 - 200,self.size.y/2),
+                                           alpha = 0)
+                                           
+        self.music_button = SpriteNode('./assets/sprites/music_button.png',
+                                       parent = self,
+                                       position = (self.size.x/2 + 200, self.size.y/2 + 100),
+                                       alpha = 1)
+                                      
+        self.musicoff_button = SpriteNode('./assets/sprites/musicoff_button.png',
+                                              parent = self,
+                                              position = (self.size.x/2 + 200,self.size.y/2),
+                                              alpha = 0)
+                                              
+    def update(self):
+        # this method is called, hopefully, 60 times a second
+        if self.music_button.alpha == 1:
+           sound.pause_effect('./assets/sounds/forgot.mp3')
+        elif self.music_button.alpha == 0:
+           sound.play_effect('./assets/sounds/forgot.mp3')
+    
+    def touch_began(self, touch):
+        # this method is called, when user touches the screen
+        if self.sound_button.frame.contains_point(touch.location):
+            self.sound_button.alpha = 0
+            self.soundoff_button.alpha = 1
+        elif self.music_button.frame.contains_point(touch.location):
+            self.music_button.alpha = 0
+            self.musicoff_button.alpha = 1
+        elif self.musicoff_button.frame.contains_point(touch.location):
+           self.musicoff_button.alpha = 0
+           self.music_button.alpha = 1
+        elif self.soundoff_button.frame.contains_point(touch.location):
+           self.soundoff_button.alpha = 0
+           self.sound_button.alpha = 1
+           
+           
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        pass
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        if self.back_button.frame.contains_point(touch.location):
+            self.dismiss_modal_scene()
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass
